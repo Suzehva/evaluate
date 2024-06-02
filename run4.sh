7671ade1a85bc4a7f3e94cfd7833f98caed4f3e2
jennyywei <jenwwwww@gmail.com>
2024-06-02 21:17:22 +0000

    add gpt 4o results

--- run4.sh
+++ run4.sh
@@ -1,29 +1,56 @@
 python main.py --model gpt-4o --sample_size 1000 --dataset math --prompting_type ""
+echo !!
 python main.py --model gpt-4o --sample_size 1000 --dataset math --prompting_type "FS" --prompting_arg 1
+echo !!
 python main.py --model gpt-4o --sample_size 1000 --dataset math --prompting_type "FS" --prompting_arg 3
+echo !!
 python main.py --model gpt-4o --sample_size 1000 --dataset math --prompting_type "COT"
+echo !!
 python main.py --model gpt-4o --sample_size 1000 --dataset math --prompting_type "COT_FS" --prompting_arg 1
+echo !!
 python main.py --model gpt-4o --sample_size 1000 --dataset math --prompting_type "COT_FS" --prompting_arg 3
+echo !!
 python main.py --model gpt-4o --sample_size 1000 --dataset math --prompting_type "TOT" --prompting_arg 3
+echo !!
 python main.py --model gpt-4o --sample_size 1000 --dataset math --prompting_type "" --use_ensemble --ensemble_size 3
+echo !!
 python main.py --model gpt-4o --sample_size 1000 --dataset math --prompting_type "FS" --prompting_arg 1 --use_ensemble --ensemble_size 3
+echo !!
 python main.py --model gpt-4o --sample_size 1000 --dataset math --prompting_type "FS" --prompting_arg 3 --use_ensemble --ensemble_size 3
+echo !!
 python main.py --model gpt-4o --sample_size 1000 --dataset math --prompting_type "COT" --use_ensemble --ensemble_size 3
+echo !!
 python main.py --model gpt-4o --sample_size 1000 --dataset math --prompting_type "COT_FS" --prompting_arg 1 --use_ensemble --ensemble_size 3
+echo !!
 python main.py --model gpt-4o --sample_size 1000 --dataset math --prompting_type "COT_FS" --prompting_arg 3 --use_ensemble --ensemble_size 3
+echo !!
 python main.py --model gpt-4o --sample_size 1000 --dataset math --prompting_type "TOT" --prompting_arg 3 --use_ensemble --ensemble_size 3
+echo !!
 python main.py --model gpt-4o --sample_size 1000 --dataset natural_questions --prompting_type ""
+echo !!
 python main.py --model gpt-4o --sample_size 1000 --dataset natural_questions --prompting_type "FS" --prompting_arg 1
+echo !!
 python main.py --model gpt-4o --sample_size 1000 --dataset natural_questions --prompting_type "FS" --prompting_arg 3
+echo !!
 python main.py --model gpt-4o --sample_size 1000 --dataset natural_questions --prompting_type "COT"
+echo !!
 python main.py --model gpt-4o --sample_size 1000 --dataset natural_questions --prompting_type "COT_FS" --prompting_arg 1
+echo !!
 python main.py --model gpt-4o --sample_size 1000 --dataset natural_questions --prompting_type "COT_FS" --prompting_arg 3
+echo !!
 python main.py --model gpt-4o --sample_size 1000 --dataset natural_questions --prompting_type "TOT" --prompting_arg 3
+echo !!
 python main.py --model gpt-4o --sample_size 1000 --dataset natural_questions --prompting_type "" --use_ensemble --ensemble_size 3
+echo !!
 python main.py --model gpt-4o --sample_size 1000 --dataset natural_questions --prompting_type "FS" --prompting_arg 1 --use_ensemble --ensemble_size 3
+echo !!
 python main.py --model gpt-4o --sample_size 1000 --dataset natural_questions --prompting_type "FS" --prompting_arg 3 --use_ensemble --ensemble_size 3
+echo !!
 python main.py --model gpt-4o --sample_size 1000 --dataset natural_questions --prompting_type "COT" --use_ensemble --ensemble_size 3
+echo !!
 python main.py --model gpt-4o --sample_size 1000 --dataset natural_questions --prompting_type "COT_FS" --prompting_arg 1 --use_ensemble --ensemble_size 3
+echo !!
 python main.py --model gpt-4o --sample_size 1000 --dataset natural_questions --prompting_type "COT_FS" --prompting_arg 3 --use_ensemble --ensemble_size 3
+echo !!
 python main.py --model gpt-4o --sample_size 1000 --dataset natural_questions --prompting_type "TOT" --prompting_arg 3 --use_ensemble --ensemble_size 3
-
+echo !!
