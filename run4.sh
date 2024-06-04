b77d50316ef1e5fd637b3f0145c5534fd3008f07
jennyywei <jenwwwww@gmail.com>
2024-06-04 21:22:17 +0000

    add 4o math results

--- run4.sh
+++ run4.sh
@@ -1,13 +1,13 @@
-python main.py --model gpt-4o --sample_size 1000 --dataset math --prompting_type ""
-echo !!
-python main.py --model gpt-4o --sample_size 1000 --dataset math --prompting_type "FS" --prompting_arg 1
-echo !!
-python main.py --model gpt-4o --sample_size 1000 --dataset math --prompting_type "FS" --prompting_arg 3
-echo !!
-python main.py --model gpt-4o --sample_size 1000 --dataset math --prompting_type "COT"
-echo !!
-python main.py --model gpt-4o --sample_size 1000 --dataset math --prompting_type "COT_FS" --prompting_arg 1
-echo !!
+# python main.py --model gpt-4o --sample_size 1000 --dataset math --prompting_type ""
+# echo !!
+# python main.py --model gpt-4o --sample_size 1000 --dataset math --prompting_type "FS" --prompting_arg 1
+# echo !!
+# python main.py --model gpt-4o --sample_size 1000 --dataset math --prompting_type "FS" --prompting_arg 3
+# echo !!
+# python main.py --model gpt-4o --sample_size 1000 --dataset math --prompting_type "COT"
+# echo !!
+# python main.py --model gpt-4o --sample_size 1000 --dataset math --prompting_type "COT_FS" --prompting_arg 1
+# echo !!
 python main.py --model gpt-4o --sample_size 1000 --dataset math --prompting_type "COT_FS" --prompting_arg 3
 echo !!
 python main.py --model gpt-4o --sample_size 1000 --dataset math --prompting_type "TOT" --prompting_arg 3
