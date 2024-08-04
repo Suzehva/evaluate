56cb6d257c99250a10f7557c444c4dd88e1a72a3
Suzehva <suzeva@stanford.edu>
2024-08-04 00:20:48 +0000

    organizing scripts

--- evaluate/scripts/error_check.sh
+++ evaluate/scripts/error_check.sh
@@ -28,3 +28,4 @@ python main.py --model gpt-3.5-turbo --sample_size 1 --dataset natural_questions
 python main.py --model gpt-3.5-turbo --sample_size 1 --dataset natural_questions --prompting_type "COT_FS" --prompting_arg 3 --use_ensemble --ensemble_size 3
 python main.py --model gpt-3.5-turbo --sample_size 1 --dataset natural_questions --prompting_type "TOT" --prompting_arg 3 --use_ensemble --ensemble_size 3
 
+
--- scripts/second_run_suze.sh
+++
@@ -1,7 +0,0 @@
-python main.py --model gpt-3.5-turbo --sample_size 1000 --dataset math --prompting_type "FS" --prompting_arg 1
-python main.py --model gpt-3.5-turbo --sample_size 1000 --dataset math --prompting_type "COT_FS" --prompting_arg 1
-python main.py --model gpt-3.5-turbo --sample_size 1000 --dataset math --prompting_type "TOT" --prompting_arg 3
-python main.py --model gpt-3.5-turbo --sample_size 1000 --dataset math --prompting_type "TOT" --prompting_arg 3 --use_ensemble --ensemble_size 3
-python main.py --model gpt-3.5-turbo --sample_size 1000 --dataset natural_questions --prompting_type "TOT" --prompting_arg 3
-python main.py --model gpt-3.5-turbo --sample_size 1000 --dataset natural_questions --prompting_type "TOT" --prompting_arg 3 --use_ensemble --ensemble_size 3
-
